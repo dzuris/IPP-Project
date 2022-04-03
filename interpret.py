98ce6aa65dbd6dafb6de866b5c7e377e2f071ccf
Dzurilla Adam <xdzuri00@merlin.fit.vutbr.cz>
2022-04-03 16:11:50 +0200

    Source and input file params

--- interpret.py
+++ interpret.py
@@ -1,22 +1,34 @@
 import sys
 
+SOURCE_FILE = ''
+INPUT_FILE = ''
+
+ERROR_INVALID_PARAMS_COMBINATION = 10
+
 def print_help_message():
 	print("Help message test")
 
 def check_arguments(arguments):
+	global SOURCE_FILE
+	global INPUT_FILE
 	for arg in arguments:
 		if arg == '--help':
 			if len(sys.argv) != 1:
-				sys.stderr.write('--help param can\'t be used with other parameters')
-				exit(10)
+				sys.stderr.write('--help param can\'t be used with other parameters\n')
+				exit(ERROR_INVALID_PARAMS_COMBINATION)
 			else:
 				print_help_message()
-	print(arg[0:9])
+		elif arg[0:9] == '--source=':
+			SOURCE_FILE=arg[9:]
+		elif arg[0:8] == '--input=':
+			INPUT_FILE=arg[8:]
 
 def main():
 	arguments = sys.argv
 	arguments.pop(0)
 	#print('Arguments: ' + str(arguments))
 	check_arguments(arguments)
+	print(SOURCE_FILE)
+	print(INPUT_FILE)
 
 main()
