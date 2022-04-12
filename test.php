927fa5e0a9039900c008f9dcee6aab1fdfa989cf
dzuris <adamdzurilla19@gmail.com>
2022-04-12 13:26:29 +0200

    test php functions and structure

--- test.php
+++ test.php
@@ -1,33 +1,21 @@
 <!--
-----------------------------------------------------------------------------------------------------
     @project Testing program
     @file test.php
     @email xdzuri00@stud.fit.vutbr.cz
     @author Adam Dzurilla
-----------------------------------------------------------------------------------------------------
 -->
 
-<!DOCTYPE html>
-<html lang="html5">
-<head>
-    <meta charset="UTF-8">
-    <title>Test output file</title>
-    <style>
-        p_red {
-            color: red;
-        }
-
-        p_green {
-            color: green;
-        }
-    </style>
-</head>
-<body>
-<h1 style="text-align:center;">
-    Results of the parse.php file
-</h1>
-<hr/>
+<!-- GLOBAL VARIABLES -->
 <?php
+    $param_parser = null;
+    $param_interpret = null;
+    $testDir = null;
+    $recursive = false;
+?>
+
+<!-- FUNCTIONS -->
+<?php
+
 function generate_error_test_head($testName)
 {
     echo "<p_red>";
@@ -56,8 +44,38 @@ function generate_expand_code($name, $file)
     echo "</section>";
 }
 
+function generate_hr() {
+    echo "<hr/>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="html5">
+<head>
+    <meta charset="UTF-8">
+    <title>Test output file</title>
+    <style>
+        p_red {
+            color: red;
+        }
+
+        p_green {
+            color: green;
+        }
+    </style>
+</head>
+<body>
+<h1 style="text-align:center;">
+    Results of the parse.php file
+</h1>
+<hr/>
+<?php
+
 generate_error_test_head("Hello");
 generate_expand_code("Expand", "inputf.txt");
+generate_hr();
+
 ?>
 </body>
 </html>
